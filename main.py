fe44edc96b562136a59b854d50ee7014c5889cd9
atrox3d <atrox3d@gmail.com>
2021-02-15 10:54:01 +0100

    csv example

---
+++ main.py
@@ -0,0 +1,15 @@
+# with open("weather_data.csv") as weather_file:
+#     data = weather_file.readlines()
+# print(data)
+import csv
+
+with open("weather_data.csv") as data_file:
+    data = csv.reader(data_file)
+    temperatures = []
+    for row in data:
+        print(row)
+        if row[1] != "temp":
+            temperatures.append(int(row[1]))
+
+    print(temperatures)
+
